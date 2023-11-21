9a8cfdfa062e0855e9564733481db40f2a1a8237
rashidintheworld <teymurlurashid@gmail.com>
2023-11-21 22:41:29 +0400

    Feature - Added validation exception and developed other methods

--- src/main/java/com/example/todobackend/exception/GeneralExceptionHandler.java
+++ src/main/java/com/example/todobackend/exception/GeneralExceptionHandler.java
@@ -3,22 +3,39 @@ package com.example.todobackend.exception;
 import com.example.todobackend.model.dto.respond.RespStatus;
 import com.example.todobackend.model.dto.respond.Response;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.Timestamp;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GeneralExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
-    public Response handleException(ResourceNotFoundException resourceNotFoundException) {
-        Response response = new Response();
-        response.setRespStatus(new RespStatus(HttpStatus.NOT_FOUND.value(), "Resource not found!"));
-        return response;
+    public ResponseEntity<ErrorResponse> handleException(ResourceNotFoundException resourceNotFoundException) {
+        ErrorResponse response = new ErrorResponse();
+        response.setStatusCode(HttpStatus.NOT_FOUND.value());
+        response.setMessage(resourceNotFoundException.getMessage());
+        response.setTimestamp(System.currentTimeMillis());
+        return new ResponseEntity<ErrorResponse>(response,HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> methodArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException){
+        ErrorResponse response = new ErrorResponse();
+        response.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        response.setMessage(methodArgumentNotValidException.getBindingResult().getFieldErrors().stream().map(x -> x.getDefaultMessage()).collect(Collectors.joining(",")));
+        response.setTimestamp(System.currentTimeMillis());
+        return new ResponseEntity<ErrorResponse>(response,HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception exception) {
+        ErrorResponse response = new ErrorResponse();
+        response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        response.setMessage(exception.getMessage());
+        response.setTimestamp(System.currentTimeMillis());
+        return new ResponseEntity<ErrorResponse>(response,HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(Exception.class)
-    public Response handleException(Exception exception) {
-        Response response = new Response();
-        response.setRespStatus(new RespStatus(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Internal server exception"));
-        return response;
-    }
 }
