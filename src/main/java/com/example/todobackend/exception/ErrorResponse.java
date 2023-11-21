84b5ae6835b69451ed42fcc9054c37a0c744f76f
rashidintheworld <teymurlurashid@gmail.com>
2023-11-21 22:39:48 +0400

    Feature - Made better error response)

---
+++ src/main/java/com/example/todobackend/exception/ErrorResponse.java
@@ -0,0 +1,19 @@
+package com.example.todobackend.exception;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private int statusCode;
+    private Long timestamp;
+    private String message;
+}
