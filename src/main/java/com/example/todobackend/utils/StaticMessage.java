a61c550368a289f0d9e1dc57e64052133ac8ea35
rashidintheworld <teymurlurashid@gmail.com>
2023-11-21 20:50:52 +0400

    Feature - added utils package for helper classes, methods etc. This method avoid bad-spagetti-confused codes. Created StaticMessage class

---
+++ src/main/java/com/example/todobackend/utils/StaticMessage.java
@@ -0,0 +1,7 @@
+package com.example.todobackend.utils;
+
+public class StaticMessage {
+    //Used for entity validation
+    public static final String NOT_NULL_MESSAGE = "This field can not be null!";
+    public static final String NOT_EMPTY_MESSAGE = "This field can not be empty!";
+}
