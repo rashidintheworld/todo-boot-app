d5f7063041bbae1adab68e35f0003e0798add722
rashidintheworld <teymurlurashid@gmail.com>
2023-12-14 01:12:18 +0400

    Feature - writed search method api

---
+++ src/main/java/com/example/todobackend/controller/TodoUtilityController.java
@@ -0,0 +1,29 @@
+package com.example.todobackend.controller;
+
+
+import com.example.todobackend.model.dto.respond.RespPage;
+import com.example.todobackend.model.dto.respond.RespTodo;
+import com.example.todobackend.model.dto.respond.Response;
+import com.example.todobackend.service.TodoUtilityService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/v1/todos")
+public class TodoUtilityController {
+    TodoUtilityService todoUtilityService;
+
+    @GetMapping("/search")
+    public Response<List<RespTodo>> searchTodos(@RequestParam(value = "query")  String query) {
+        return todoUtilityService.searchTodos(query);
+    }
+
+
+
+}
