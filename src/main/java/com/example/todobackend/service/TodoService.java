3673758bbca2cb174a2a2f76ed0f471e2422141e
rashidintheworld <teymurlurashid@gmail.com>
2023-12-13 17:41:31 +0400

    Feature - added Todo Service class for write service methods

---
+++ src/main/java/com/example/todobackend/service/TodoService.java
@@ -0,0 +1,20 @@
+package com.example.todobackend.service;
+
+import com.example.todobackend.model.dto.request.TodoDTO;
+import com.example.todobackend.model.dto.respond.RespTodo;
+import com.example.todobackend.model.dto.respond.Response;
+import com.example.todobackend.model.entity.Todo;
+
+import java.util.List;
+
+public interface TodoService {
+    Response<RespTodo> getTodo(Long id);
+    Response<RespTodo> saveTodo(TodoDTO todoDTO);
+    Response<List<RespTodo>> getAllTodos();
+    Response deleteTodo(Long id);
+    Response updateTodo(Long id, TodoDTO todoDTO);
+
+    //additional
+    void completeTodo(Long id);
+    void inCompleteTodo(Long id);
+}
