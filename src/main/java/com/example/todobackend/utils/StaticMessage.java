c230aa7e17b6024bb4b143f349afc1e34be2f215
rashidintheworld <teymurlurashid@gmail.com>
2023-12-13 17:47:10 +0400

    Feature - added one more static message to avoid boilerplate code

--- src/main/java/com/example/todobackend/utils/StaticMessage.java
+++ src/main/java/com/example/todobackend/utils/StaticMessage.java
@@ -4,4 +4,10 @@ public class StaticMessage {
     //Used for entity validation
     public static final String NOT_NULL_MESSAGE = "This field can not be null!";
     public static final String NOT_EMPTY_MESSAGE = "This field can not be empty!";
+
+    //Error messages
+    public static final String NOT_FOUND_MESSAGE = "Not Found!";
+    public static final String ACTIVE_WARN_MESSAGE = "Todo is not active!";
+
+
 }
