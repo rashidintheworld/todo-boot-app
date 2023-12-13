7294c4f3ad58fdedd34d4fb952d3a22eb9350596
rashidintheworld <teymurlurashid@gmail.com>
2023-12-14 01:05:45 +0400

    Feature - added interface to write abstract utility methods

---
+++ src/main/java/com/example/todobackend/service/TodoUtilityService.java
@@ -0,0 +1,11 @@
+package com.example.todobackend.service;
+
+import com.example.todobackend.model.dto.respond.RespPage;
+import com.example.todobackend.model.dto.respond.RespTodo;
+import com.example.todobackend.model.dto.respond.Response;
+
+import java.util.List;
+
+public interface TodoUtilityService {
+    Response<List<RespTodo>> searchTodos(String query);
+}
