fbf89b2ba56452e73f7d2aceec87a86e55f499c4
rashidintheworld <teymurlurashid@gmail.com>
2023-12-14 01:03:24 +0400

    Feature - added interface

---
+++ src/main/java/com/example/todobackend/repository/TodoUtilityRepository.java
@@ -0,0 +1,16 @@
+package com.example.todobackend.repository;
+
+import com.example.todobackend.model.entity.Role;
+import com.example.todobackend.model.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TodoUtilityRepository extends JpaRepository<Todo,Long> {
+    @Query("SELECT p FROM Todo p WHERE " +
+            "p.title LIKE CONCAT('%', :query, '%')" + "Or p.description LIKE CONCAT('%', :query, '%')")
+    List<Todo> searchProducts(String query);
+}
