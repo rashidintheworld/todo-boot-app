16d7b6d8e7aa5a122f85566f57050fc95189129f
rashidintheworld <teymurlurashid@gmail.com>
2023-12-14 01:28:34 +0400

    Feature - added sort abstract method

--- src/main/java/com/example/todobackend/service/TodoUtilityService.java
+++ src/main/java/com/example/todobackend/service/TodoUtilityService.java
@@ -1,5 +1,6 @@
 package com.example.todobackend.service;
 
+import com.example.todobackend.enums.SortDirectionEnum;
 import com.example.todobackend.model.dto.respond.RespPage;
 import com.example.todobackend.model.dto.respond.RespTodo;
 import com.example.todobackend.model.dto.respond.Response;
@@ -8,5 +9,5 @@ import java.util.List;
 
 public interface TodoUtilityService {
     Response<List<RespTodo>> searchTodos(String query);
-    
+    Response<List<RespTodo>> getAllTodosSortedByTitle(SortDirectionEnum sortDirection);
 }
